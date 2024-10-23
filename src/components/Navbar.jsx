96bd0210f0a3158162287859d3e7526729525090
ajiangriawan <ajiangriawan6@gmail.com>
2024-10-23 17:02:19 +0700

    versi1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+import { logo } from "../assets/images";
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <NavLink to='/'>
+        <p className='w-18 h-18 object-contain border-l-blue-50 text-white bg-blue-500 rounded p-2 font-bold'>Aji Angri Awan</p>
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
